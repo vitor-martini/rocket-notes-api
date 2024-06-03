8c24ac1fbcdbd016103f178aaaef9e6d2c7896c1
Vitor Martini <vitor-martini@outlook.com>
2024-06-02 21:07:10 -0300

    feat: CORS

--- src/server.js
+++ src/server.js
@@ -4,10 +4,12 @@ const routes = require("./routes");
 const AppError = require("./utils/AppError.js")
 const migrationsRun = require("./database/sqlite/migrations")
 const uploadConfig = require("./configs/upload.js");
+const cors = require("cors");
 
 const app = express();
 const PORT = 3333;
 
+app.use(cors()); 
 app.use(express.json()); 
 app.use(routes)
 app.use("/files", express.static(uploadConfig.UPLOADS_FOLDER));
